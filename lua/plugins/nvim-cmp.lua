c3a88481cc515a6b276bfde2296d5f2e2ff10f1a
Reblixt <carl@klofverskjold.se>
2024-07-18 10:52:19 +0200

    Removed some mini code inside CMP that broke the editor

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -142,72 +142,72 @@ return {
     or { import = "lazyvim.plugins.extras.coding.luasnip", enabled = vim.fn.has("nvim-0.10") == 0 },
 
   -- auto pairs
-  {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    opts = {
-      mappings = {
-        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-      },
-    },
-    keys = {
-      {
-        "<leader>up",
-        function()
-          vim.g.minipairs_disable = not vim.g.minipairs_disable
-          if vim.g.minipairs_disable then
-            LazyVim.warn("Disabled auto pairs", { title = "Option" })
-          else
-            LazyVim.info("Enabled auto pairs", { title = "Option" })
-          end
-        end,
-        desc = "Toggle Auto Pairs",
-      },
-    },
-  },
+  -- {
+  --   "echasnovski/mini.pairs",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     mappings = {
+  --       ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+  --     },
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>up",
+  --       function()
+  --         vim.g.minipairs_disable = not vim.g.minipairs_disable
+  --         if vim.g.minipairs_disable then
+  --           LazyVim.warn("Disabled auto pairs", { title = "Option" })
+  --         else
+  --           LazyVim.info("Enabled auto pairs", { title = "Option" })
+  --         end
+  --       end,
+  --       desc = "Toggle Auto Pairs",
+  --     },
+  --   },
+  -- },
 
   -- comments
-  {
-    "folke/ts-comments.nvim",
-    event = "VeryLazy",
-    opts = {},
-    enabled = vim.fn.has("nvim-0.10") == 1,
-  },
-  {
-    import = "lazyvim.plugins.extras.coding.mini-comment",
-    enabled = vim.fn.has("nvim-0.10") == 0,
-  },
+  -- {
+  --   "folke/ts-comments.nvim",
+  --   event = "VeryLazy",
+  --   opts = {},
+  --   enabled = vim.fn.has("nvim-0.10") == 1,
+  -- },
+  -- {
+  --   import = "lazyvim.plugins.extras.coding.mini-comment",
+  --   enabled = vim.fn.has("nvim-0.10") == 0,
+  -- },
 
   -- Better text-objects
-  {
-    "echasnovski/mini.ai",
-    event = "VeryLazy",
-    opts = function()
-      LazyVim.on_load("which-key.nvim", function()
-        vim.schedule(LazyVim.mini.ai_whichkey)
-      end)
-      local ai = require("mini.ai")
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-          o = ai.gen_spec.treesitter({ -- code block
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-          d = { "%f[%d]%d+" }, -- digits
-          e = { -- Word with case
-            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
-            "^().*()$",
-          },
-          i = LazyVim.mini.ai_indent, -- indent
-          g = LazyVim.mini.ai_buffer, -- buffer
-          u = ai.gen_spec.function_call(), -- u for "Usage"
-          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
-        },
-      }
-    end,
-  },
+  -- {
+  --   "echasnovski/mini.ai",
+  --   event = "VeryLazy",
+  --   opts = function()
+  --     LazyVim.on_load("which-key.nvim", function()
+  --       vim.schedule(LazyVim.mini.ai_whichkey)
+  --     end)
+  --     local ai = require("mini.ai")
+  --     return {
+  --       n_lines = 500,
+  --       custom_textobjects = {
+  --         o = ai.gen_spec.treesitter({ -- code block
+  --           a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+  --           i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+  --         }),
+  --         f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+  --         c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+  --         t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+  --         d = { "%f[%d]%d+" }, -- digits
+  --         e = { -- Word with case
+  --           { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+  --           "^().*()$",
+  --         },
+  --         i = LazyVim.mini.ai_indent, -- indent
+  --         g = LazyVim.mini.ai_buffer, -- buffer
+  --         u = ai.gen_spec.function_call(), -- u for "Usage"
+  --         U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+  --       },
+  --     }
+  --   end,
+  -- },
 }
